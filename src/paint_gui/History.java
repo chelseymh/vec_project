f5137a478bd4a38d85c26dd036270adf3076db87
faith <faith.lim@connect.qut.edu.au>
2019-05-28 18:44:23 +1000

    Chelsey's undo history squashed and rebased

---
+++ src/paint_gui/History.java
@@ -0,0 +1,73 @@
+package paint_gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class History {
+    Canvas canvas;
+    public String labels[];
+    private List<String> tempCommands = new ArrayList<String>();
+    private List<String> userSelection= new ArrayList<String>();
+
+    public History(Canvas canvas) {
+        this.canvas = canvas;
+    }
+
+    public void fillLabels() {
+        tempCommands.clear(); // Clear the Jlist on open to ensure OLD / UNDOED drawings are removed
+        for (String string : canvas.getCommands()) { // go through commands and add them to a tempList
+            tempCommands.add(string);
+        }
+        for (int i = 0; i < canvas.getCommands().size(); i++) { // Go through tempList and convert to Array to display
+            labels = tempCommands.toArray(new String[0]);
+        }
+    }
+
+    //displays a preview of what undo history would look
+    //like if applied
+    public void  displayPreview(int index){
+        //clear final list for usage
+        userSelection.clear();
+        List<String> originalCommands= new ArrayList<String>();
+        //calls the actual commands object in canvas
+        List<String> commands =canvas.getCommands();
+        // Backing up old commands
+        originalCommands.addAll(commands);
+
+        //remove all commands from the end till user selection
+        //select(index);
+        int i=commands.size()-1;
+        while (i > index) {
+            //remove commmand
+            commands.remove(i);
+            i--;
+        }
+
+        //save the list in case user wants this
+        userSelection.addAll(commands);
+
+
+        canvas.clean();
+        canvas.readCommands();
+        canvas.repaint();
+
+        //clear commands
+        commands.clear();
+        //put back in the original list
+        //for more user previewing
+        commands.addAll(originalCommands);
+    }
+
+    public void windowCloseAction() {
+        System.out.println("History closed, applying changes...");
+        List<String> commands =canvas.getCommands();
+        //clear the old commands list
+        commands.clear();
+        //update with the users final decision
+        commands.addAll(userSelection);
+        canvas.clean();
+        canvas.readCommands();
+        canvas.repaint();
+    }
+}
