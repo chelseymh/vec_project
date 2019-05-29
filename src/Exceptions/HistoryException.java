30fc6d8339e5e209e4ee4c0d801ba2f7171fcadb
Chelsey Hansson <chelsey.hansson@gmail.com>
2019-05-29 17:43:05 +1000

    Added history exception class

---
+++ src/Exceptions/HistoryException.java
@@ -0,0 +1,9 @@
+package Exceptions;
+
+import paint_gui.History;
+
+public class HistoryException extends Exception {
+    public HistoryException(String message) {
+        super(message);
+    }
+}
