9333e430c850a5fc846e9b4bf646d480b7ff84bc
MaiQUT <n10411551@qut.edu.au>
2019-05-31 17:42:05 +1000

    Added comments to InvalidDimensionsException

--- src/Exceptions/InvalidDimensionsException.java
+++ src/Exceptions/InvalidDimensionsException.java
@@ -1,6 +1,13 @@
 package Exceptions;
 
+/**
+ * Checked exception to signal that the input given for a set of dimensions is incorrect.
+ */
 public class InvalidDimensionsException extends Exception {
+    /**
+     * Creates an <code>InvalidDimensionsException</code> with the specified detail message.
+     * @param message The detail message (which is saved for later retrieval by the <code>getMessage</code> method from the <code>Throwable</code> class)
+     */
     public InvalidDimensionsException(String message) {
         super(message);
     }
