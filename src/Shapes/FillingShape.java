5dc8abe20a54e632998b58b7a728a1bbe318286d
MaiQUT <n10411551@qut.edu.au>
2019-06-02 17:51:27 +1000

    Added comments

--- src/Shapes/FillingShape.java
+++ src/Shapes/FillingShape.java
@@ -2,6 +2,13 @@ package Shapes;
 
 import java.awt.*;
 
+/**
+ * Specifies the fill method of the shapes that need to implement this functionality.
+ */
 public interface FillingShape {
+    /**
+     * Fills a shape by using the fill methods of the <code>java.awt</code> package.
+     * @param g the Graphics2D tool the shape is to be filled
+     */
     void fill(Graphics2D g);
 }
