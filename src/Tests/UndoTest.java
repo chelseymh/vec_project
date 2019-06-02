d2a313bd38e0f195a3238eaa85d737a994766ef8
MaiQUT <n10411551@qut.edu.au>
2019-06-02 19:43:10 +1000

    Changed test classes to fit with new changes. Canvas doesn't take a colour and Undo is undo

--- src/Tests/UndoTest.java
+++ src/Tests/UndoTest.java
@@ -1,36 +1,56 @@
 package Tests;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import paint_gui.Undo;
 import paint_gui.Canvas;
-import Exceptions.UndoException;
-
 import java.awt.*;
 import java.util.Random;
+import Exceptions.UndoException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UndoTest {
-    Undo undo;
-    private static Canvas canvas = new Canvas(Color.white);
-//    @BeforeEach
-//    void setUp() {
-//        undo = null;
-//    }
 
+    private Canvas canvas; // Is this meant to be static?
+    private Undo undo;
 
-    //Tests
-    @Test
-    void test1() throws UndoException {
-    undo = new Undo(canvas);
-        try { undo.Undo();}
-        catch(UndoException e)
-        {assertEquals("No more undoes", e.getMessage());}
+    @BeforeEach
+    void setUp() {
+        canvas = new Canvas();
+        undo = new Undo(canvas);
     }
-    
+    @AfterEach
+    void tearDown() {
+    }
 
+    /** TEST THE FOLLOWING **/
 
+    @Test
+    void testUndo() throws UndoException { // Throws null pointer due to clean() and readCommands()
+        canvas.addCommand("Drawing 1");
+        canvas.addCommand("Drawing 2");
+        undo.undo();
+        int size = canvas.getCommands().size();
+        assertEquals(size, 1);
+    }
 
+    @Test
+    void testNoMoreUndoes() throws UndoException {
+        try {
+            undo.undo();
+        } catch(UndoException e) {
+            assertEquals("No more undoes", e.getMessage());}
+    }
+
+    @Test
+    void testMultipleUndo() throws  UndoException { // Throws null pointer due to clean() and readCommands()
+        canvas.addCommand("Drawing 1");
+        canvas.addCommand("Drawing 2");
+        undo.undo();
+        int size = canvas.getCommands().size();
+        assertEquals(size, 0);
+    }
 
 }
